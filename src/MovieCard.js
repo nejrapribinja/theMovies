2c32a7351881682ce687125182157d1ee35d1402
Nejra <nejra.pribinja@gmail.com>
2023-03-11 13:56:35 +0100

    first commit

--- src/MovieCard.js
+++ src/MovieCard.js
@@ -1,15 +1,15 @@
-const MovieCard = () => {
+const MovieCard = ({ name, first_air_date, poster_path }) => {
   return (
     <div class=" me-3 ">
       <img
-        src={"https://www.themoviedb.org/t/p/w220_and_h330_face/xsW7M4b4gawgFKCzcXHL2MSeswj.jpg"}
+        src={`https://www.themoviedb.org/t/p/w220_and_h330_face${poster_path}`}
         width="150"
         height="225"
         alt="..."
       />
       <div>
-        <h5>Card title</h5>
-        <p>17/02/1999</p>
+        <h6>{name}</h6>
+        <p>{first_air_date}</p>
       </div>
     </div>
   );
--- src/Movies.js
+++ src/Movies.js
@@ -1,22 +1,29 @@
+import { useEffect, useState } from "react";
 import MovieCard from "./MovieCard";
+import axios from "axios";
 
 const Movies = () => {
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.themoviedb.org/3/tv/popular?api_key=c6d6659e774322031d29a692fe261b8e&language=en-US&page=1"
+      )
+      .then((response) => {
+        setMovies(response.data.results);
+        console.log(movies);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
   return (
     <div class="container mt-4 mb-4">
       <h3>Movies</h3>
       <div class="d-flex  overflow-x-scroll">
-        <MovieCard />
-        <MovieCard />
-        <MovieCard />
-        <MovieCard />
-        <MovieCard />
-        <MovieCard />
-        <MovieCard />
-        <MovieCard />
-        <MovieCard />
-        <MovieCard />
-        <MovieCard />
-        <MovieCard />
+        {movies.map((movie, index) => {
+          return <MovieCard key={movie.id} {...movie} />;
+        })}
       </div>
     </div>
   );
